78bf45f352e13420a108968a3407e896fe3ec133
Charlotte Pawloski <143750577+UnseenObserver@users.noreply.github.com>
2025-05-13 13:31:56 -0400

    upgrade fix

--- CodieClicker/UpgradeView.swift
+++ CodieClicker/UpgradeView.swift
@@ -8,7 +8,8 @@
 import SwiftUICore
 import SwiftUI
 
-struct upgrade {
+struct upgrade: Hashable {
+    
     var name: String
     var add: Bool
     var value: Int
@@ -19,20 +20,39 @@ struct upgrade {
         self.value = value
     }
     
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    
 }
 
 struct UpgradeView: View {
     @ObservedObject var clicker: Clickable
-    var upgradesAvaliable: [upgrade] = [upgrade(name: "Add 1", add: true, value: 1), upgrade(name: "Add 2", add: true, value: 2)]
+    @State var upgradesAvaliable: [upgrade] = []
     
     
     var body: some View {
         HStack{
             VStack{
                 List {
-                    
+                    ForEach(upgradesAvaliable, id: \.self) { upgrade in
+                        Button(upgrade.name) {
+                            if upgrade.add {
+                                clicker.upgradeAdd += upgrade.value
+                            } else {
+                                clicker.upgradeMult += upgrade.value
+                            }
+                        }
+                    }
                 }
             }
         }
+        .onAppear() {
+            makeArray()
+        }
+    }
+    
+    func makeArray() {
+        upgradesAvaliable = [upgrade(name: "Add 1", add: true, value: 1), upgrade(name: "Add 2", add: true, value: 2), upgrade(name: "Add 3", add: true, value: 3)]
     }
 }
