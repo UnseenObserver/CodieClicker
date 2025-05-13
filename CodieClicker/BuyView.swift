d265d72a5dd6835c09bb7ebbbe933e2c81f5bf99
Charlotte Pawloski <143750577+UnseenObserver@users.noreply.github.com>
2025-05-13 14:11:33 -0400

    Helo

--- CodieClicker/BuyView.swift
+++ CodieClicker/BuyView.swift
@@ -18,13 +18,26 @@ struct BuyView: View {
             VStack{
                 List {
                     Button("AutoClicker") {
-                        var cost: Int = 10
+                        let cost: Int = 10
                         if clicker.amountOfData >= cost {
                             clicker.amountOfData = clicker.amountOfData - cost
                             autoClickOn = true
                         }
                     }
                 }
+            }
+        }
+    }
+}
+
+struct buyable: View {
+    @State var name: String
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .center) {
+                Image(systemName: "pencil")
+                Text(name)
             }
         }
     }
