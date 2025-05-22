26145f7c01f9038081d0d9f2715b8b516662d935
Charlotte Pawloski <charlottepawloski@gmail.com>
2025-05-22 14:42:51 -0400

    done

---
+++ CodieClicker/TestView.swift
@@ -0,0 +1,57 @@
+//
+//  TestView.swift
+//  CodieClicker
+//
+//  Created by HPro2 on 5/22/25.
+//
+
+import SwiftUICore
+import SwiftUI
+
+struct TestView: View {
+    
+    var body: some View {
+        HStack {
+            VStack{
+                ScrollView(.horizontal) {
+                    HStack(alignment: .top) {
+                        //                            ForEach(0..<100) {
+                        //                                Text("Row \($0)")
+                        //                            }
+                        //                        }
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                        Text("Hello")
+                    }
+                    }
+            }
+        }
+    }
+}
